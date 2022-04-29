7965aea1f342834fd468ae9e7df9461ba380177f
João <101433764+Joaopefull@users.noreply.github.com>
2022-04-29 14:03:52 -0300

    Add files via upload

---
+++ exemplo.js
@@ -0,0 +1,10 @@
+function ola (){
+    const a = 0;    
+}
+function foo(){
+    var x , y = 0
+    
+    console.log(x + y + 1);
+    console.log(x + y + 1);
+    console.log(x + y + 1);   
+}
---
+++ main.js
@@ -0,0 +1,60 @@
+
+
+/*
+var d = new Date();
+alert(d.getMinutes());
+alert(d.getMonths());
+alert(d.getYears());
+alert(d.getDays());
+alert(d.getHours());
+
+/*
+var count;
+for(count=0; count <=5; count++){
+  alert(count);
+}
+
+/*
+var count = 5;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count++
+};
+/*
+var idade = prompt ("Qual sua idade");
+if(idade >- 18){
+  alert ("maior de idade");
+}else{
+    alert("menor de idade");
+};
+
+//var fruta = [{nome: "maça" , cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+//console.log(frutas);
+//alert(fruta[1].nome);
+
+/*
+var fruta = {nome: "maça" , cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop()
+
+    //console.log(lista[0]);
+    //console.log (lista.toString(0));
+    //console.log (lista. join(" - "));
+
+
+//var nome = "Django Livre";
+//var n1 = 33;
+//var n2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert (nome + " tem " + idade + " anos");
+//alert (n1 + n2);
+//console.log(nome);
+//console.log(n1 + n2);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
